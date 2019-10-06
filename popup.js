c5554073869cfed12f2007ca5710e34957dbb914
ych Dev <devych.code@gmail.com>
2019-10-06 23:35:41 +0900

    addButtonOnPopup

--- popup.js
+++ popup.js
@@ -1,4 +1,9 @@
 console.log("123");
+let page = document.getElementById("buttonDiv");
+let Button = document.createElement("button");
+
+page.appendChild(Button);
+
 function showSomething() {
   let popupProp = "color";
   for (let i = 0; i < 5; i++) {
